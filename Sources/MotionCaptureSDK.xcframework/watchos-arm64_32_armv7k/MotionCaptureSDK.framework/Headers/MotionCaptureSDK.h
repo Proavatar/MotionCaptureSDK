6315db49ec6eddad022facf6e1ec01eec005c796
Fred Dijkstra <fred@computerguided.com>
2022-06-27 21:59:39 +0200

    Framework rebuilt

---
+++ Sources/MotionCaptureSDK.xcframework/watchos-arm64_32_armv7k/MotionCaptureSDK.framework/Headers/MotionCaptureSDK.h
@@ -0,0 +1,18 @@
+//
+//  MotionCaptureSDK.h
+//  MotionCaptureSDK
+//
+//  Created by Fred Dijkstra on 22/06/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MotionCaptureSDK.
+FOUNDATION_EXPORT double MotionCaptureSDKVersionNumber;
+
+//! Project version string for MotionCaptureSDK.
+FOUNDATION_EXPORT const unsigned char MotionCaptureSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MotionCaptureSDK/PublicHeader.h>
+
+
